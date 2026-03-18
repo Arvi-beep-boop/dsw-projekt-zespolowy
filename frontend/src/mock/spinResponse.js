92f5c05e6f600a773167a511a77ebd140128f9f6
Seevi <Sevtka92@gmail.com>
2026-03-19 00:21:31 +0100

    feat: add mock data structure

---
+++ frontend/src/mock/spinResponse.js
@@ -0,0 +1,16 @@
+export const mockSpinData = {
+  "grid": [
+    [1, 1, 1],
+    [6, 3, 2],
+    [4, 1, 1]
+  ],
+  "win": 20,
+  "winLineData": [
+    {
+      "symbol": 1,
+      "length": 3,
+      "winLineId": 0,
+      "win": 50
+    }
+  ]
+};
